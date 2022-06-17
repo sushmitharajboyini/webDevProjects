9ffc870ee972a593c62c1addeab8cba1bce4cc82
sushmitharajboyini <105933759+sushmitharajboyini@users.noreply.github.com>
2022-06-17 15:47:59 +0530

    Add files via upload

---
+++ Toggle Project/script.js
@@ -0,0 +1,26 @@
+const toggles = document.querySelectorAll(".toggle");
+
+const good = document.querySelector("#good");
+const cheap = document.querySelector("#cheap");
+const fast = document.querySelector("#fast");
+
+console.log(good);
+
+toggles.forEach((toggle) => toggle.addEventListener("change", (e) => {
+  console.log(e);
+  console.log(e.target);
+  transitionFunc(e.target)
+}))
+function transitionFunc(theClickedOne){
+  if(good.checked && fast.checked && cheap.checked){
+    if(good === theClickedOne){
+      fast.checked = false;
+    }
+    if(cheap===theClickedOne){
+      good.checked= false;
+    }
+    if(fast===theClickedOne){
+      cheap.checked = false;
+    }
+  }
+}
